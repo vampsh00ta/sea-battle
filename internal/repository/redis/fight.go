0ab11d1f57a23175268f2e55ef5b5762af212c1d
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2024-03-26 15:58:21 +0300

    decreased getting battlefields  latency

--- internal/repository/redis/fight.go
+++ internal/repository/redis/fight.go
@@ -45,18 +45,38 @@ func (r Redis) GetFight(ctx context.Context, sessionId string) (models.Fight, er
 	if err := r.client.HGetAll(ctx, sessionId).Scan(&session); err != nil {
 		return models.Fight{}, err
 	}
-	user1, err := r.GetUser(ctx, session.TgId1)
-	if err != nil {
-		return models.Fight{}, err
+
+	wg := &sync.WaitGroup{}
+	res := make(chan error, 2)
+	tgIDs := []string{
+		session.TgId1,
+		session.TgId2,
 	}
-	user2, err := r.GetUser(ctx, session.TgId2)
-	if err != nil {
-		return models.Fight{}, err
+	resUsers := make([]models.User, 2)
+	for i, tgID := range tgIDs {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, tgID string, i int) {
+			defer wg.Done()
+			user, err := r.GetUser(ctx, tgID)
+			res <- err
+			resUsers[i] = user
+
+		}(wg, tgID, i)
+
+	}
+
+	wg.Wait()
+	close(res)
+
+	for err := range res {
+		if err != nil {
+			return models.Fight{}, err
+		}
 	}
 
 	fight := models.Fight{
-		User1: user1,
-		User2: user2,
+		User1: resUsers[0],
+		User2: resUsers[1],
 		Turn:  session.Turn,
 		State: session.Ready,
 	}
