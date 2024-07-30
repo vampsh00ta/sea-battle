e5d451c1ddbb702e78aabd9af2b4f4ad6afce8d7
vampsh00ta <trofimov.vlad-1234@yandex.ru>
2024-07-30 20:04:27 +0300

    deleted search method and deed InitFight method

--- internal/app/service/fight.go
+++ internal/app/service/fight.go
@@ -7,11 +7,11 @@ import (
 
 type Fight interface {
 	Shoot(ctx context.Context, req entity.Shoot) (entity.Fight, int, error)
-	//AddShip(ctx context.Context, tgId string, p1, p2 entity.Point) error
 	JoinFight(ctx context.Context, code, tgId string) (entity.Fight, error)
+	InitFight(ctx context.Context, tgIDs ...string) (entity.Fight, error)
+
 	CreateFight(ctx context.Context, tgId string) (string, error)
 	SetShip(ctx context.Context, req entity.SetShip) (*entity.BattleField, int, error)
 	SetFieldQueryId(ctx context.Context, sessionId, tgId, queryId string, my bool) error
-	//SearchFight(ctx context.Context, tgId int) error
 	InitFightAction(ctx context.Context, token string) (*entity.Fight, error)
 }
